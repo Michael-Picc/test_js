8664a27ddc5301924473cda6c3165a67a7c7e5a2
Michael-Picc <136484545+Michael-Picc@users.noreply.github.com>
2024-09-10 15:54:42 -0500

    Create success.js

---
+++ success.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('Always Succeed Test', function() {
+  it('should always pass', function() {
+    assert.strictEqual(true, true); // This assertion will always pass
+  });
+});
