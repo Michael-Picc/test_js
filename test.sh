13e62c0ebc475deb120b1cdd644fe7000e69c625
Michael-Picc <136484545+Michael-Picc@users.noreply.github.com>
2024-09-13 08:08:38 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo "This is a test"
-cat success.js | grep Assert
+echo "This is a test" >> output.txt
+cat success.js | grep Assert >> output.txt
 
 # Define an assert function
 assert() {
   local condition="$1"
   local message="$2"
+  local log_file="$3"
   
   if ! eval "$condition"; then
-    echo "Assertion failed: $message"
+    echo "Assertion failed: $message" >> output.txt
     exit 1
   fi
 }
-
 #function to test
 add() {
   local a="$1"
@@ -30,4 +30,3 @@ assert "[ \"$result\" -eq 0 ]" "Expected add(0, 0) to equal 0, but got $result"
 result=$(add -1 1)
 assert "[ \"$result\" -eq 0 ]" "Expected add(-1, 1) to equal 0, but got $result"
 
-echo "All tests passed!"
